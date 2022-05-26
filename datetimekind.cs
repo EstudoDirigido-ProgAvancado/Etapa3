bf2251cb67554d2ff0afcf61ab4d0225e24c984b
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2022-05-26 00:07:46 -0300

    Update datetimekind.cs

--- datetimekind.cs
+++ datetimekind.cs
@@ -1,3 +1,5 @@
+Import System;
+
 DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
 DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
 DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
